7d4dcc78f495757df23543f9a038823375771124
Shubham <bluestacks@192.168.0.3>
2022-09-12 19:46:30 +0530

    terraform

---
+++ eks-creation/providers.tf
@@ -0,0 +1,6 @@
+ provider "aws" {
+       region = var.region
+       access_key = "AKIAVFFDLJTH6CX6FOVI"
+       secret_key = "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"
+   }
+ 
---
+++ eks-creation/variables.tf
@@ -0,0 +1,28 @@
+variable "region" {
+    default = "ap-south-1"
+}
+
+
+
+variable "vpc_id" {
+    default = "vpc-0b91e7afe2f20968a"
+}   
+
+variable "cluster_name" {
+    default = "mycluster"
+}
+
+variable "node_group_name" {
+    default = "mynode"
+}
+
+variable "node_instance_type" {
+    default = "t2.micro"
+}
+
+
+
+
+
+
+
